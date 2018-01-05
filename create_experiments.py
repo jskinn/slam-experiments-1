9f78e38e805a24759bbb07aacbbb6a8ec5ea90af
John Skinner <jskinn7@gmail.com>
2018-01-05 13:21:23 +1000

    Add the kitti verify experiment to the list.

--- create_experiments.py
+++ create_experiments.py
@@ -1,11 +1,13 @@
 import arvet.create_experiment
 import simple_motion_experiment
 import real_world_experiment
+import orbslam_kitti_verify
 
 
 def main():
     arvet.create_experiment.create_experiment(simple_motion_experiment.SimpleMotionExperiment)
     arvet.create_experiment.create_experiment(real_world_experiment.RealWorldExperiment)
+    arvet.create_experiment.create_experiment(orbslam_kitti_verify.OrbslamKITTIVerify)
 
 
 if __name__ == '__main__':
