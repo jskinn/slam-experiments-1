204a44723d3d593111f49720326fdbc4139cae6f
John Skinner <jskinn7@gmail.com>
2018-01-27 19:14:06 +1000

    Add origins for AIUE_V01_005

--- euroc_generated_data_experiment.py
+++ euroc_generated_data_experiment.py
@@ -215,6 +215,7 @@ def get_MH_01_easy():
     return [
         ('AIUE_V01_002', uetf.create_serialized((2240, 415, 75), (0, 0, 160))),
         ('AIUE_V01_004', uetf.create_serialized((485, -45, 145), (0, 0, 145))),
+        ('AIUE_V01_005', uetf.create_serialized((-925, -1135, 120), (0, 0, 0))),
     ]
 
 
@@ -222,11 +223,14 @@ def get_MH_02_easy():
     return [
         ('AIUE_V01_002', uetf.create_serialized((2240, 415, 75), (0, 0, 156))),
         ('AIUE_V01_004', uetf.create_serialized((225, 0, 145), (0, 0, 140))),
+        ('AIUE_V01_005', uetf.create_serialized((770, 180, 60), (0, 0, 180))),
     ]
 
 
 def get_MH_03_medium():
-    return []
+    return [
+        ('AIUE_V01_005', uetf.create_serialized((-490, -450, 275), (0, 0, -90))),
+    ]
 
 
 def get_MH_04_difficult():
@@ -234,7 +238,9 @@ def get_MH_04_difficult():
 
 
 def get_MH_05_difficult():
-    return []
+    return [
+        ('AIUE_V01_005', uetf.create_serialized((210, -770, -60), (0, 0, 140))),
+    ]
 
 
 def get_V1_01_easy():
@@ -245,6 +251,7 @@ def get_V1_01_easy():
         ('AIUE_V01_002', uetf.create_serialized((-40, -175, 135), (0, 0, -75))),
         ('AIUE_V01_003', uetf.create_serialized((-115, 520, 135), (0, 0, 155))),
         ('AIUE_V01_004', uetf.create_serialized((385, 0, 425), (0, 0, -105))),
+        ('AIUE_V01_005', uetf.create_serialized((-550, -85, 125), (0, 0, 135))),
     ]
 
 
@@ -256,6 +263,7 @@ def get_V1_02_medium():
         ('AIUE_V01_002', uetf.create_serialized((-7.5, -190, 105), (0, 0, -75))),
         ('AIUE_V01_003', uetf.create_serialized((-115, 520, 135), (0, 0, 155))),
         ('AIUE_V01_004', uetf.create_serialized((405, 10, 105), (0, 0, -90))),
+        ('AIUE_V01_005', uetf.create_serialized((-560, -1305, 480), (0, 0, -175))),
     ]
 
 
@@ -265,6 +273,7 @@ def get_V1_03_difficult():
         ('AIUE_V01_002', uetf.create_serialized((-522.5, 60, 260), (0, 0, -135))),
         ('AIUE_V01_003', uetf.create_serialized((-235, 255, 135), (0, 0, 155))),
         ('AIUE_V01_004', uetf.create_serialized((-230, -135, 435), (0, 0, 0))),
+        ('AIUE_V01_005', uetf.create_serialized((-70, -495, 120), (0, 0, 70))),
     ]
 
 
@@ -274,6 +283,7 @@ def get_V2_01_easy():
         ('AIUE_V01_002', uetf.create_serialized((367.5, -95, 120), (0, 0, -105))),
         ('AIUE_V01_003', uetf.create_serialized((-345, 560, 135), (0, 0, -70))),
         ('AIUE_V01_004', uetf.create_serialized((-350, -35, 205), (0, 0, 115))),
+        ('AIUE_V01_005', uetf.create_serialized((-490, -450, 100), (0, 0, -90))),
     ]
 
 
@@ -282,6 +292,7 @@ def get_V2_02_medium():
         ('AIUE_V01_002', uetf.create_serialized((-355, -10, 205), (0, 0, -170))),
         ('AIUE_V01_003', uetf.create_serialized((-345, 560, 170), (0, 0, -90))),
         ('AIUE_V01_004', uetf.create_serialized((405, 10, 105), (0, 0, -90))),
+        ('AIUE_V01_005', uetf.create_serialized((-540, -290, 195), (0, 0, 150))),
     ]
 
 
@@ -290,4 +301,5 @@ def get_V2_03_difficult():
         ('AIUE_V01_002', uetf.create_serialized((-355, -10, 205), (0, 0, -170))),
         ('AIUE_V01_003', uetf.create_serialized((-345, 560, 160), (0, 0, -110))),
         ('AIUE_V01_004', uetf.create_serialized((-695, -30, 430), (0, 0, -5))),
+        ('AIUE_V01_005', uetf.create_serialized((-540, -290, 195), (0, 0, 150))),
     ]
--- tum_generated_data_experiment.py
+++ tum_generated_data_experiment.py
@@ -216,6 +216,9 @@ def get_frieburg1_360():
         ('AIUE_V01_002', uetf.create_serialized((-990, -135, 315), (0, 0, 25))),
         ('AIUE_V01_003', uetf.create_serialized((-105, -335, 135), (0, 0, 110))),
         ('AIUE_V01_003', uetf.create_serialized((-70, 635, 135), (0, 0, -140))),
+
+        ('AIUE_V01_005', uetf.create_serialized((-95, -695, 65), (0, 0, 150))),
+        ('AIUE_V01_005', uetf.create_serialized((-795, -1490, 490), (0, 0, 180))),
     ]
 
 def get_frieburg1_rpy():
@@ -227,6 +230,9 @@ def get_frieburg1_rpy():
         ('AIUE_V01_002', uetf.create_serialized((-990, -135, 315), (0, 0, 25))),
         ('AIUE_V01_003', uetf.create_serialized((-105, -335, 135), (0, 0, 110))),
         ('AIUE_V01_003', uetf.create_serialized((-70, 635, 135), (0, 0, -140))),
+
+        ('AIUE_V01_005', uetf.create_serialized((-95, -695, 65), (0, 0, 150))),
+        ('AIUE_V01_005', uetf.create_serialized((-795, -1490, 490), (0, 0, 180))),
     ]
 
 def get_frieburg1_xyz():
@@ -238,6 +244,9 @@ def get_frieburg1_xyz():
         ('AIUE_V01_002', uetf.create_serialized((-990, -135, 315), (0, 0, 25))),
         ('AIUE_V01_003', uetf.create_serialized((-105, -335, 135), (0, 0, 110))),
         ('AIUE_V01_003', uetf.create_serialized((-70, 635, 135), (0, 0, -140))),
+
+        ('AIUE_V01_005', uetf.create_serialized((-95, -695, 65), (0, 0, 150))),
+        ('AIUE_V01_005', uetf.create_serialized((-795, -1490, 490), (0, 0, 180))),
     ]
 
 def get_frieburg2_desk():
@@ -246,6 +255,8 @@ def get_frieburg2_desk():
         ('AIUE_V01_001', uetf.create_serialized((-235, -255, 80), (0, 0, 180))),
         ('AIUE_V01_002', uetf.create_serialized((-50, 175, 90), (0, 0, 175))),
         ('AIUE_V01_003', uetf.create_serialized((-20, 70, 105), (0, 0, 180))),
+
+        ('AIUE_V01_005', uetf.create_serialized((-775, -475, 15), (0, 0, 0))),
     ]
 
 def get_frieburg2_rpy():
@@ -257,6 +268,9 @@ def get_frieburg2_rpy():
         ('AIUE_V01_002', uetf.create_serialized((-990, -135, 315), (0, 0, 25))),
         ('AIUE_V01_003', uetf.create_serialized((-105, -335, 135), (0, 0, 110))),
         ('AIUE_V01_003', uetf.create_serialized((-70, 635, 135), (0, 0, -140))),
+
+        ('AIUE_V01_005', uetf.create_serialized((-95, -695, 65), (0, 0, 150))),
+        ('AIUE_V01_005', uetf.create_serialized((-795, -1490, 490), (0, 0, 180))),
     ]
 
 def get_frieburg2_xyz():
@@ -268,6 +282,9 @@ def get_frieburg2_xyz():
         ('AIUE_V01_002', uetf.create_serialized((-990, -135, 315), (0, 0, 25))),
         ('AIUE_V01_003', uetf.create_serialized((-105, -335, 135), (0, 0, 110))),
         ('AIUE_V01_003', uetf.create_serialized((-70, 635, 135), (0, 0, -140))),
+
+        ('AIUE_V01_005', uetf.create_serialized((-95, -695, 65), (0, 0, 150))),
+        ('AIUE_V01_005', uetf.create_serialized((-795, -1490, 490), (0, 0, 180))),
     ]
 
 def get_frieburg3_structure_texture_far():
@@ -276,6 +293,8 @@ def get_frieburg3_structure_texture_far():
         ('AIUE_V01_001', uetf.create_serialized((-220, 640, 100), (0, 0, 0))),
         ('AIUE_V01_002', uetf.create_serialized((-115, -315, 95), (0, 0, 15))),
         ('AIUE_V01_003', uetf.create_serialized((-20, 70, 105), (0, 0, 180))),
+
+        ('AIUE_V01_005', uetf.create_serialized((430, -250, 145), (0, 0, -165))),
     ]
 
 def get_frieburg3_walking_xyz():
@@ -287,4 +306,7 @@ def get_frieburg3_walking_xyz():
         ('AIUE_V01_002', uetf.create_serialized((-990, -135, 315), (0, 0, 25))),
         ('AIUE_V01_003', uetf.create_serialized((-105, -335, 135), (0, 0, 110))),
         ('AIUE_V01_003', uetf.create_serialized((-70, 635, 135), (0, 0, -140))),
+
+        ('AIUE_V01_005', uetf.create_serialized((-95, -695, 65), (0, 0, 150))),
+        ('AIUE_V01_005', uetf.create_serialized((-795, -1490, 490), (0, 0, 180))),
     ]
