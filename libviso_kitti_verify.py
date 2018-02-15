f2d5f3a6476675db6dd4712eaac48d8044f5625a
John Skinner <jskinn7@gmail.com>
2018-02-15 20:39:42 +1000

    Reduce repeats on libviso experiments.

--- libviso_kitti_verify.py
+++ libviso_kitti_verify.py
@@ -24,7 +24,7 @@ class LibVisOKITTIVerify(base_verify.VerificationExperiment):
         :param enabled:
         :param id_:
         """
-        super().__init__(systems=systems, datasets=datasets, benchmarks=benchmarks, repeats=10,
+        super().__init__(systems=systems, datasets=datasets, benchmarks=benchmarks, repeats=1,
                          id_=id_, trial_map=trial_map, result_map=result_map, enabled=enabled)
 
     def do_imports(self, task_manager: arvet.batch_analysis.task_manager.TaskManager,
