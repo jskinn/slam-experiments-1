72a713324f405fb42ad9fbe481c1ea24b012d755
John Skinner <jskinn7@gmail.com>
2018-03-02 17:59:03 +1000

    Add a plot of orbslam consistency over time

--- orbslam_consistency_experiment.py
+++ orbslam_consistency_experiment.py
@@ -109,16 +109,158 @@ class OrbslamConsistencyExperiment(arvet.batch_analysis.simple_experiment.Simple
         :return:
         """
         # Visualize the different trajectories in each group
+        self._plot_variance_vs_time(db_client)
         self._plot_estimate_variance(db_client)
         self._plot_error_vs_motion(db_client)
         # self._plot_variations(db_client)
+
+    def _plot_variance_vs_time(self, db_client: arvet.database.client.DatabaseClient):
+        import matplotlib.pyplot as pyplot
+        # noinspection PyUnresolvedReferences
+        from mpl_toolkits.mplot3d import Axes3D
+
+        save_path = os.path.join('figures', type(self).__name__, 'variance vs time')
+        os.makedirs(save_path, exist_ok=True)
+
+        logging.getLogger(__name__).info("Plotting variance vs time and saving to {0} ...".format(save_path))
+
+        for dataset_name, dataset_id in self.datasets.items():
+            for system_name, system_id in self.systems.items():
+                logging.getLogger(__name__).info("    .... distributions for {0} on {1}".format(system_name,
+                                                                                                dataset_name))
+                times = []
+                x_variance = []
+                y_variance = []
+                z_variance = []
+
+                trial_result_list = self.get_trial_results(system_id, dataset_id)
+                ground_truth_traj = None
+                gt_scale = None
+                computed_trajectories = []
+                timestamps = []
+
+                if len(trial_result_list) <= 0:
+                    continue
+
+                # Collect all the trial results
+                for trial_result_id in trial_result_list:
+                    trial_result = dh.load_object(db_client, db_client.trials_collection, trial_result_id)
+                    if trial_result is not None and trial_result.success:
+                        if ground_truth_traj is None:
+                            ground_truth_traj = trial_result.get_ground_truth_camera_poses()
+                            gt_scale = th.find_trajectory_scale(ground_truth_traj)
+                        traj = trial_result.get_computed_camera_poses()
+
+                        # Normalize monocular trajectories
+                        if 'mono' in system_name.lower():
+                            if gt_scale is not None:
+                                traj = th.rescale_trajectory(traj, gt_scale)
+                            else:
+                                logging.getLogger(__name__).warning("Cannot rescale trajectory, missing ground truth")
+
+                        computed_trajectories.append(traj)
+                        timestamps.append({k: v for k, v in ass.associate(ground_truth_traj, traj,
+                                                                          max_difference=0.1, offset=0)})
+
+                # Now that we have all the trajectories, we can measure consistency
+                current_pose = None
+                for time in sorted(ground_truth_traj.keys()):
+                    if sum(1 for idx in range(len(timestamps)) if time in timestamps[idx]) <= 1:
+                        # Skip locations/results which appear in only one trajectory
+                        continue
+
+                    # Find the distance to the prev frame
+                    prev_pose = current_pose
+                    current_pose = ground_truth_traj[time]
+                    if prev_pose is not None:
+                        # Find the mean estimated location for this time
+                        computed_motions = [
+                            prev_pose.find_relative(computed_trajectories[idx][timestamps[idx][time]]).location
+                            for idx in range(len(computed_trajectories))
+                            if time in timestamps[idx]
+                        ]
+                        mean_computed_motion = np.mean(computed_motions, axis=0)
+                        times += [time for _ in range(len(computed_motions))]
+                        x_variance += [computed_motion[0] - mean_computed_motion[0]
+                                       for computed_motion in computed_motions]
+                        y_variance += [computed_motion[1] - mean_computed_motion[1]
+                                       for computed_motion in computed_motions]
+                        z_variance += [computed_motion[2] - mean_computed_motion[2]
+                                       for computed_motion in computed_motions]
+
+                # Plot error vs motion in that direction
+                title = "{0} on {1} estimate variation".format(system_name, dataset_name)
+                figure = pyplot.figure(figsize=(30, 10), dpi=80)
+                figure.suptitle(title)
+
+                ax = figure.add_subplot(131)
+                ax.set_title('x')
+                ax.set_xlabel('time (s)')
+                ax.set_ylabel('x variance (m)')
+                ax.plot(times, x_variance, c='blue', alpha=0.5, marker='.', markersize=2, linestyle='None')
+
+                ax = figure.add_subplot(132)
+                ax.set_title('y')
+                ax.set_xlabel('time (s)')
+                ax.set_ylabel('y variance (m)')
+                ax.plot(times, y_variance, c='blue', alpha=0.5, marker='.', markersize=2, linestyle='None')
+
+                ax = figure.add_subplot(133)
+                ax.set_title('z')
+                ax.set_xlabel('time (s)')
+                ax.set_ylabel('z variance (m)')
+                ax.plot(times, z_variance, c='blue', alpha=0.5, marker='.', markersize=2, linestyle='None')
+
+                pyplot.tight_layout()
+                pyplot.subplots_adjust(top=0.95, right=0.99)
+
+                figure.savefig(os.path.join(save_path, title + '.png'))
+                pyplot.close(figure)
+
+                # Plot error vs time as a histogram
+                title = "{0} on {1} estimate variation heatmap".format(system_name, dataset_name)
+                figure = pyplot.figure(figsize=(30, 10), dpi=80)
+                figure.suptitle(title)
+
+                ax = figure.add_subplot(131)
+                ax.set_title('x')
+                ax.set_xlabel('time (s)')
+                ax.set_ylabel('x variance (m)')
+                heatmap, xedges, yedges = np.histogram2d(times, x_variance, bins=300)
+                ax.imshow(heatmap.T, extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]], origin='lower',
+                          aspect='auto', cmap=pyplot.get_cmap('inferno_r'))
+
+                ax = figure.add_subplot(132)
+                ax.set_title('y')
+                ax.set_xlabel('time (s)')
+                ax.set_ylabel('y variance (m)')
+                heatmap, xedges, yedges = np.histogram2d(times, y_variance, bins=300)
+                ax.imshow(heatmap.T, extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]], origin='lower',
+                          aspect='auto', cmap=pyplot.get_cmap('inferno_r'))
+
+                ax = figure.add_subplot(133)
+                ax.set_title('z')
+                ax.set_xlabel('time (s)')
+                ax.set_ylabel('z variance (m)')
+                heatmap, xedges, yedges = np.histogram2d(times, z_variance, bins=300)
+                ax.imshow(heatmap.T, extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]], origin='lower',
+                          aspect='auto', cmap=pyplot.get_cmap('inferno_r'))
+
+                pyplot.tight_layout()
+                pyplot.subplots_adjust(top=0.95, right=0.99)
+
+                figure.savefig(os.path.join(save_path, title + '.png'))
+                pyplot.close(figure)
+
+        # Show all the graphs remaining
+        pyplot.show()
 
     def _plot_estimate_variance(self, db_client: arvet.database.client.DatabaseClient):
         import matplotlib.pyplot as pyplot
         # noinspection PyUnresolvedReferences
         from mpl_toolkits.mplot3d import Axes3D
 
-        save_path = os.path.expanduser('~/Pictures/orbslam variance heatmaps')
+        save_path = os.path.join('figures', type(self).__name__, 'orbslam variance heatmaps')
         os.makedirs(save_path, exist_ok=True)
 
         logging.getLogger(__name__).info("Plotting error vs motion and saving to {0} ...".format(save_path))
@@ -249,7 +391,7 @@ class OrbslamConsistencyExperiment(arvet.batch_analysis.simple_experiment.Simple
         # noinspection PyUnresolvedReferences
         from mpl_toolkits.mplot3d import Axes3D
 
-        save_path = os.path.expanduser('~/Pictures/orbslam error vs motion heatmaps')
+        save_path = os.path.join('figures', type(self).__name__, 'orbslam error vs motion heatmaps')
         os.makedirs(save_path, exist_ok=True)
 
         logging.getLogger(__name__).info("Plotting error vs motion and saving to {0} ...".format(save_path))
